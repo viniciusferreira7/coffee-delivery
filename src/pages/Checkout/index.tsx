1f0cf54a3f36073c1e27a211a550c54712da869d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-10-16 22:04:20 -0300

    feat(checkout): making the HTML5 part of the checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,44 @@
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+import {
+  CheckoutContainer,
+  FormContainer,
+  SectionFormContainer,
+  SectionPaymentContainer,
+} from './styles'
+
 export function Checkout() {
-  return <div>Checkout</div>
+  return (
+    <CheckoutContainer>
+      <SectionFormContainer>
+        <h2>Complete seu pedido</h2>
+        <article>
+          <MapPinLine size={32} weight="regular" />
+          <h3>Endereço de Entrega</h3>
+          <p>Informe o endereço onde deseja receber seu pedido</p>
+        </article>
+        <FormContainer>
+          <input type="text" placeholder="CEP" />
+          <input type="text" placeholder="Rua" />
+          <div>
+            <input type="number" placeholder="Numero" />
+            <input type="text" placeholder="Complemento" />
+          </div>
+          <div>
+            <input type="text" placeholder="Bairro" />
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" />
+          </div>
+        </FormContainer>
+      </SectionFormContainer>
+      <SectionPaymentContainer>
+        <article>
+          <CurrencyDollar size={22} />
+          <h2>Pagamento</h2>
+          <p>
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </p>
+        </article>
+      </SectionPaymentContainer>
+    </CheckoutContainer>
+  )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const CheckoutContainer = styled.main``
+export const CheckoutContainer = styled.main`
+  max-width: 90rem;
+
+  margin: auto;
+`
+export const SectionFormContainer = styled.section``
+
+export const FormContainer = styled.form``
+
+export const SectionPaymentContainer = styled.section``
