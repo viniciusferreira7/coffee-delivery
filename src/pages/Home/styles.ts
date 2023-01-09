f2e2484ba15cf165f263be8462a610d7718e0e1b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-01-09 14:18:19 -0300

    style(home.ts): correção do espaçamento

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const HomeContainer = styled.main`
   margin: auto;
   margin-top: 6.5rem;
+
+  @media screen and (max-width: 768px) {
+    padding: 0 2%;
+  }
 `
